5c21efe477453d2161bce44c3b09f8525521090f
Maicol Rodrigo <maicolcrodrigoa@gmail.com>
2021-05-30 11:22:17 -0500

    cloud linux installer

--- centos7/cloudlinux.sh
+++ centos7/cloudlinux.sh
@@ -2,11 +2,15 @@
 
 {
   if [ -f .env ]; then
-      # Load Environment Variables
+      # Carga las variable del .env
       export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
   fi
 
+  echo "[DEBUG] Starts running the installer dependiencies" >> /var/log/cloudlinux.log
+
   yum install wget -y
+
+  echo "[DEBUG] Dependiencies successfully installed" >> /var/log/cloudlinux.log
 
   echo "[DEBUG] Start download installer cloudlinux" >> /var/log/cloudlinux.log
 
@@ -20,6 +24,24 @@
 
   echo "[DEBUG] Cloudlinux successfully installed" >> /var/log/cloudlinux.log
 
-  reboot
+  echo "[DEBUG] Starts running CageFs installer" >> /var/log/cloudlinux.log
+
+  yum install cagefs -y
+
+  echo "[DEBUG] CageFs successfully installed" >> /var/log/cloudlinux.log
+
+  # Necesita de una la cualquier palabra que contenga numero 1 inicialmente
+  LIMIT=$(cat /etc/yachay/cloudlinux.cfg)
+  SUB="3"
+
+  if [[ "$LIMIT" != *"$SUB"* ]]; then
+    NUM=$((${LIMIT: -1} + 1))
+    echo "[DEBUG] Attempt number $NUM"
+  fi
+
+  echo $LIMIT$NUM > /etc/yachay/cloudlinux.cfg
+
 
 } 2>&1 | tee -a /var/log/cloudlinux.process.log
+
+# Obtener el log de la ejecucion 2>&1 | tee -a /var/log/cloudlinux.process.log
