b22a008b4a4306fdef3ec285e9c292242f3dc2e4
Maicol Rodrigo <maicolcrodrigoa@gmail.com>
2021-06-20 13:18:15 -0500

    letsencrypt

---
+++ ubuntu/letsencrypt/nginx/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import fileinput
+from dotenv import dotenv_values
+import shutil
+
+config = dotenv_values(".env")
+
+textToSearch = "$DOMAIN"
+textToReplace = config['DOMAIN']
+fileToSearch = "nginx.conf"
+
+tempFile = open( fileToSearch, 'r+' )
+
+for line in fileinput.input( fileToSearch ):
+    tempFile.write( line.replace( textToSearch, textToReplace ) )
+tempFile.close()
+
+nginxFile = '/etc/nginx/sites-available/' + fileToSearch
+# nginxFile = 'other/default'
+
+shutil.copy(fileToSearch, nginxFile)
+
+cronFile = '/var/spool/cron/crontabs/root'
+# cronFile = 'other/root'
+
+with open(cronFile, 'a') as f:
+    f.write('\n0 12 * * * /usr/bin/certbot renew --quiet')
