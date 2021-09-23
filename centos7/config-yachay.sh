cb76dceb1b775db5ac4f5a0020cb6d20d089bc48
Maicol C Rodrigo <maicolcrodrigoa@gmail.com>
2021-09-23 02:14:33 -0500

    agregar scripts de yachay

--- centos7/config-yachay.sh
+++ centos7/config-yachay.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 echo "[DEBUG] Create workdir" >> /var/log/config-yachay.log
 
@@ -17,12 +18,51 @@ wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/cpanel
 
 echo "[DEBUG] Finish download config files" >> /var/log/config-yachay.log
 
+echo "[DEBUG] Start file relocation" >> /var/log/config-yachay.log
+
+mv /etc/csf/csf.allow /etc/csf/csf.allow.bak
+
+mv /etc/csf/csf.conf /etc/csf/csf.conf.bak
+
+mv /etc/csf/csf.ignore /etc/csf/csf.ignore.bak
+
 cp csf.* /etc/csf/
 
-csf -r
+csf -r >> /var/log/csf-yachay.log
+
+CSF_YACHAY=$(cat /var/log/csf-yachay.log | grep "Error")
+
+if [ "$CSF_YACHAY" ]
+then
+    echo "[ERROR] Se ha producido un error en la reubicación del archivo CSF, compruebe el archivo /var/log/csf-yachay.log."
+else
+    echo "[SUCCESS] The CSF files were successfully relocated."
+fi
+
+mv /etc/exim.conf.local /etc/exim.conf.local.bak
 
 cp exim.conf.local /etc/
 
-/scripts/buildeximconf
+/scripts/buildeximconf >> /var/log/exim-build-yachay.log
 
-/scripts/restartsrv_exim
+/scripts/restartsrv_exim >> /var/log/exim-restart-yachay.log
+
+BUILD_EXIM_YACHAY=$(cat /var/log/exim-build-yachay.log | grep "Error")
+
+if [ "$BUILD_EXIM_YACHAY" ]
+then
+    echo "[ERROR] There was an error relocating the EXIM file, check the /var/log/exim-build-yachay.log file."
+else
+    echo "[SUCCESS] EXIM files were successfully relocated."
+fi
+
+RESTART_EXIM_YACHAY=$(cat /var/log/exim-restart-yachay.log | grep "Error")
+
+if [ "$RESTART_EXIM_YACHAY" ]
+then
+    echo "[ERROR] An error occurred while restarting the EXIM service, check the /var/log/exim-restart-yachay.log file."
+else
+    echo "[SUCCESS] EXIM service restarted successfully"
+fi
+
+echo "[DEBUG] File relocation is completed" >> /var/log/config-yachay.log
---
+++ centos7/init-package-whm.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+echo "Enter domain or subdomain: "
+
+read SUBDOMAIN
+
+SUM=0
+
+EXPRESION="^[A-Za-z0-9]+$"
+
+for i in $(echo $SUBDOMAIN | tr "." "\n")
+do
+  if [[ $i =~ $EXPRESION ]];
+  then
+    SUM=$(expr $SUM + 1)
+  else
+    echo "[ERROR] Invalid domain or subdomain"
+    exit 1
+  fi
+done
+
+if [ "$SUM" -le 1 ] || [ "$SUM" -ge 4 ];
+then
+    echo "[ERROR] Invalid domain or subdomain"
+    exit 1
+fi
+
+echo "Enter your Cloudlinux license: "
+
+read LICENCE_CLOUDLINUX
+
+if [[ "$LICENCE_CLOUDLINUX" != CL-* ]]
+then
+  echo "[ERROR] Invalid license"
+  exit 1
+fi
+
+echo "Enter your LiteSpeed Web Server license (TRIAL/you_key): "
+
+read LICENCE_LITESPEED
+
+# START SCRIPT INSTALL APPS
+
+mkdir -p /etc/yachay
+
+echo "Linux1" > /etc/yachay/cloudlinux.cfg
+
+CLOUD_INIT=/etc/yachay/cloud-init
+
+mkdir -p $CLOUD_INIT
+
+cd $CLOUD_INIT
+
+echo "SUBDOMAIN=$SUBDOMAIN" >> .env
+
+wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/whm.sh
+
+chmod +x whm.sh
+
+sh whm.sh TRIAL
+
+# echo -e "password1234\npassword1234" | passwd root
+
+echo "LICENCE_CLOUDLINUX=$LICENCE_CLOUDLINUX" >> .env
+
+wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/
+cloudlinux.sh
+
+chmod +x /etc/yachay/cloud-init/cloudlinux.sh
+
+sh cloudlinux.sh
+
+wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/
+config-yachay.sh
+
+chmod +x /etc/yachay/cloud-init/config-yachay.sh
+
+sh config-yachay.sh
+
+reboot
