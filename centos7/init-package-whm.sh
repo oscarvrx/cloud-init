4d4b1ac4657847a3b4e5f8f5c7c9834e9cee6257
Maicol C Rodrigo <maicolcrodrigoa@gmail.com>
2021-10-04 09:41:09 -0500

    reparar ejecucion de scripts

--- centos7/init-package-whm.sh
+++ centos7/init-package-whm.sh
@@ -57,7 +57,7 @@ wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/whm.sh
 
 chmod +x whm.sh
 
-# sh whm.sh TRIAL
+sh whm.sh TRIAL
 
 # echo -e "password1234\npassword1234" | passwd root
 
@@ -67,12 +67,12 @@ wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/cloudl
 
 chmod +x /etc/yachay/cloud-init/cloudlinux.sh
 
-# sh cloudlinux.sh
+sh cloudlinux.sh
 
 wget https://raw.githubusercontent.com/oscarvrx/cloud-init/master/centos7/config-yachay.sh
 
 chmod +x /etc/yachay/cloud-init/config-yachay.sh
 
-# sh config-yachay.sh
+sh config-yachay.sh
 
 reboot
